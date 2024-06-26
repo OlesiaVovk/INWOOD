1bb6982306d5e835208a2c287a36e81c3db99ff8
Olesia Vovk <2kirkeugle@gmail.com>
2024-06-26 13:16:25 +0600

    Исправлены названия картинок

---
+++ scripts/explore.js
@@ -0,0 +1,28 @@
+a = [
+  {
+    name: "Bedroom",
+    elements: [
+      {
+        imageURL: "./assets/test1.jpg",
+        link: "/somelink",
+      },
+      {
+        imageURL: "./assets/test2.jpg",
+        link: "/somelink2",
+      },
+    ],
+  },
+  {
+    name: "Kitchen",
+    elements: [
+      {
+        imageURL: "./assets/test3.jpg",
+        link: "/somelink3",
+      },
+      {
+        imageURL: "./assets/test4.jpg",
+        link: "/somelink4",
+      },
+    ],
+  },
+];
